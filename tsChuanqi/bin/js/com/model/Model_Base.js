760cc9d3adb7acf27968cdeb2265855f5cf0b014
Anteins <377023491@qq.com>
2017-09-06 18:29:24 +0800

    remove ./bin/js

--- tsChuanqi/bin/js/com/model/Model_Base.js
+++
@@ -1,9 +0,0 @@
-var EIGame;
-(function (EIGame) {
-    var Model_Base = (function () {
-        function Model_Base() {
-        }
-        return Model_Base;
-    }());
-    EIGame.Model_Base = Model_Base;
-})(EIGame || (EIGame = {}));
